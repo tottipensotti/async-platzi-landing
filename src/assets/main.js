d0f8392f4682a5820ce9f119a503c704729fdf95
tottipensotti <ag.pensotti@gmail.com>
2023-01-15 15:05:01 -0300

    commit

---
+++ src/assets/main.js
@@ -0,0 +1,45 @@
+// import fetch from 'node-fetch';
+
+const API = 'https://crypto-news16.p.rapidapi.com/news/cointelegraph';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f01b6fa3a4msh12c2ce33302e83ap1a50fcjsne5194b78ae4e',
+		'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+	}
+};
+
+const content = null || document.querySelector('#content');
+
+async function fetchData(url) {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const news = await fetchData(API);
+        let view = `
+            ${news.map(article => `
+            <div class="group relative">
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">${article.date}</h3>
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">${article.title}</h3>
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">${article.description}</h3>
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <a href="${article.url}" target="_blank">Read full note</a>
+                </div>
+            </div>
+            `).slice(0, 12).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
